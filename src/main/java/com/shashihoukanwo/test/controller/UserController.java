defa99c51588aadc71770baad1ce413138633ac0
dengh <2039601051@qq.com>
2016-01-26 14:41:43 +0800

    添加了日志

--- src/main/java/com/shashihoukanwo/test/controller/UserController.java
+++ src/main/java/com/shashihoukanwo/test/controller/UserController.java
@@ -1,11 +1,14 @@
 package com.shashihoukanwo.test.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.shashihoukanwo.test.entity.User;
 import com.shashihoukanwo.test.service.UserService;
 
 @Controller
@@ -13,10 +16,13 @@ import com.shashihoukanwo.test.service.UserService;
 public class UserController {
 	@Autowired
 	private UserService userService; 
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
 	@RequestMapping("/getUser")
 	public ModelAndView getUser(@RequestParam("id") int id){
-		ModelAndView mv = new ModelAndView("/test/user");
-		mv.addObject("user", userService.getUserById(id));
+		ModelAndView mv = new ModelAndView("test/user");
+		User user = userService.getUserById(id);
+		mv.addObject("user", user);
+		logger.info(user.toString());
 		return mv;
 	}
 }
