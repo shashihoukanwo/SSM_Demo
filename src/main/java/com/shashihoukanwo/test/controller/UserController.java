931ab05c4d31007ec27d1a130c09e672b61fd7f1
dengh <2039601051@qq.com>
2016-01-26 13:31:25 +0800

    新创建

---
+++ src/main/java/com/shashihoukanwo/test/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.shashihoukanwo.test.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.shashihoukanwo.test.service.UserService;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	private UserService userService; 
+	@RequestMapping("/getUser")
+	public ModelAndView getUser(@RequestParam("id") int id){
+		ModelAndView mv = new ModelAndView("/user.jsp");
+		mv.addObject("user", userService.getUserById(id));
+		return mv;
+	}
+}
